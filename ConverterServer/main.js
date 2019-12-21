0312c9826d01559f4f2bb2a7f1d5cc1a47db9aa6
Accalina <ikhsanbayu.uplink@gmail.com>
2019-12-22 00:27:34 +0700

    Added initial API Server

---
+++ ConverterServer/main.js
@@ -0,0 +1,19 @@
+
+const port = 8081
+
+const express   = require('express')
+const bp        = require('body-parser')
+const morgan    = require('morgan')
+
+const app = express()
+
+app.use(bp.json())
+app.use(bp.urlencoded({extended: true}))
+app.use(morgan('tiny'))
+
+require('./src/routes/endpoints')(app);
+
+
+app.listen(port ,()=> {
+    console.log(`Server listin on port ${port}`)
+})
